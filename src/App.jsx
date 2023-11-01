4b97ae99cc22b57c411033dea733434f25b92673
AliievLomachMaxim <aliev.lomach.maxim@gmail.com>
2023-11-01 21:52:51 +0200

    add less

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import GetArrElem from './components/GetArrElem'
+import Wrapper from './components/Wrapper'
+
+const App = () => {
+	// return GetArrElem({ age: 20, name: 'Alex' })
+	return (
+		<div>
+			<p>qwrety</p>
+			<h2>qwery</h2>
+			<GetArrElem age='20' name='Alex' />
+			<Wrapper>QQQQQ</Wrapper>
+		</div>
+	)
+}
+
+export default App
---
+++ src/components/Wrapper/index.jsx
@@ -0,0 +1,5 @@
+const Wrapper = (props) => {
+	return <h1>{props.children}</h1>
+}
+
+export default Wrapper
